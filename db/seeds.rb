4cf9cc9a875e66110a5a159e9ef124e5a04729d2
fahia mohamed <fahia_mohamed@hotmail.com>
2014-07-26 18:42:11 -0400

    created seeds to add teachers to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,12 @@
 
 100.times{Feedback.create(title: ["dishes","cleaning","EE", "Instructors", "Curriculum"].sample, content: Faker::Lorem.word, user_id: rand(1..50))}
 
-
+#seeded small sample of teachers for feedback 
+User.create(username: "Sam Blackman", password_digest: "password", cohort: "")
+User.create(username: "Alex Flores", password_digest: "password", cohort: "")
+User.create(username: "Nikola Savic", password_digest: "password", cohort: "")
+User.create(username: "Rebecca Miller-Webster", password_digest: "password", cohort: "")
+User.create(username: "Tanner Welsh", password_digest: "password", cohort: "")
 
 # 25.times{Feedback.create(title: ["Antonio","cleaning","EE", "Party options", "Daily naps"].sample, content: Faker::Lorem.paragraph, teacher_id: rand(26..50))}
 
