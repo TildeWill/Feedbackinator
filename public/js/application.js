26e098f02c5962166935042da8fd86768cf3570d
fahiamoh <fahia_mohamed@hotmail.com>
2014-07-27 19:14:52 -0400

    adding jquery to the navbar

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,12 @@
-$(document).ready(function() {
+$(function() {
+    $('#navigation a').stop().animate({'marginLeft':'-85px'},1000);
+
+    $('#navigation > li').hover(
+        function () {
+            $('a',$(this)).stop().animate({'marginLeft':'-2px'},200);
+        },
+        function () {
+            $('a',$(this)).stop().animate({'marginLeft':'-85px'},200);
+        }
+    );
 });
