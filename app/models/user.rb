5dd97f790d2e151bdb4e9b085bd38d65031052d4
Sid Patel <sidpatel13@gmail.com>
2014-07-27 14:09:39 -0400

    questionnaire done, add logic so a user can only submit one questionnaire

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :feedbacks
   has_many :subfeedbacks, through: :feedbacks
+  has_one :questionnaire
 #class method called .teachers and .students 
 
 end
