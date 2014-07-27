104726be0615130e74cff1a2b01b9d5279993f71
hilarybarr <hilaryrosebarr@gmail.com>
2014-07-27 15:33:23 -0400

    made routes more RESTFUL

--- app/controllers/feedback.rb
+++ app/controllers/feedback.rb
@@ -4,15 +4,16 @@ get '/feedback' do
   erb :"feedback/feedback_form"	
 end
 
-get '/yourfeedback' do
+get '/users/:id/feedbacks' do
   @categories = Category.all
+
 
   erb :"your_feedback"	
 end
 
 post '/feedback' do
   @category = Category.find_by_name(params[:your_category])
-  current_user.feedbacks.create(title: params[:title], content: params[:feedback], category: @category.id, private_public: params[:private])
+  current_user.feedbacks.create(title: params[:title], content: params[:feedback], category_id: @category.id, private_public: params[:private])
   redirect '/'
 end
 
@@ -41,12 +42,32 @@ end
 # 	return @feedback.to_json
 # end
 
-
+get '/delete/feedback/:id' do
+  @feedback = Feedback.find(params[:id])
+  @feedback.destroy
+  redirect to ('/yourfeedback/:id')
+end
 
 
 get '/feedback/:id' do
   @feedback = Feedback.find(params[:id])
 
   erb :"feedback/specific_feedback"
+end
+
+
+
+post '/posts/:id/comments' do
+  post_id = params[:id] 
+  if logged_in? == true
+    user_id = session[:user_id]
+    @post = Post.find(post_id)
+    @comment = Comment.create(body: params[:comment], user_id: user_id)
+    @post.comments << @comment
+    @comment.body.to_s
+    erb :'post/comment', layout: false
+  else
+    redirect "/posts/#{post_id}"
+  end
 end
 
